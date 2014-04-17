add5d997f7c08b7d3223f16df89f374caf64e26b
Lee Williams <williamsl@CFPB-rMBP15-Williams.(none)>
2014-04-17 10:36:20 -0400

    got tangents working

--- static/js/main.js
+++ static/js/main.js
@@ -5,72 +5,106 @@ $( document ).ready(function() {
                   context.canvas.height = $("#canvas-container").height();
 
 
-  var center = { 
+  var center, mousePos, vector, p1, p2, p3, p4, radius, beamCenter;
+
+  center = { 
     x: context.canvas.width/2,
     y: context.canvas.height/2
   };
 
-  var mousePos = {
+  mousePos = {
       x: 0,
       y: 0
     };
 
-  var vector = {
+  vector = {
     x: center.x - mousePos.x,
     y: center.y - mousePos.y
   }
 
-  var p1 = {
+  p1 = {
     x: center.x + vector.x * Math.cos(getRadians(9)),
     y: center.y + vector.y * Math.sin(getRadians(9))
   }
 
-  var p2 = {
+  p2 = {
     x: center.x + vector.x * Math.sin(getRadians(9)),
     y: center.y + vector.y * Math.cos(getRadians(9))
   }
 
-  var beamCenter = {
+  beamCenter = {
     x: (p1.x + p2.x)/2,
     y: (p1.y + p2.y)/2
   }
 
-  var perpVector = {
-    x: (-(p2.y - p1.y))/2,
-    y: (p2.x - p1.x)/2
+  radius = getDist(p1.x, p1.y, p2.x, p2.y)/2;
+
+  dist = {
+    x: beamCenter.x - mousePos.x,
+    y: beamCenter.y - mousePos.y
   }
 
-  var p3 = {
-    x: beamCenter.x + perpVector.x,
-    y: beamCenter.y + perpVector.y,
+  //tangents
+
+  var dist, dd, a, b, t, ta, tb;
+
+  dist = {
+    x: beamCenter.x - mousePos.x,
+    y: beamCenter.y - mousePos.y
   }
 
-  var p4 = {
-    x: beamCenter.x - perpVector.x,
-    y: beamCenter.y - perpVector.y,
+  dd = Math.sqrt(dist.x * dist.x + dist.y * dist.y);
+
+  a = Math.asin(radius / dd);
+  b = Math.atan2(dist.y, dist.x);
+
+  t = b - a;
+  ta = { 
+    x:radius * Math.sin(t), 
+    y:radius * -Math.cos(t) 
+  };
+    
+  t = b + a;
+  tb = { 
+    x:radius * -Math.sin(t), 
+    y:radius * Math.cos(t) 
+  };
+
+  p3 = {
+    x: beamCenter.x + ta.x,
+    y: beamCenter.y + ta.y
+  }
+
+  p4 = {
+    x: beamCenter.x + tb.x,
+    y: beamCenter.y + tb.y
   }
 
 
+  context.beginPath();
+  context.moveTo(mousePos.x, mousePos.y);
+  context.lineTo(center.x,center.y);
+  context.stroke();
+  
+  context.beginPath();
+  context.moveTo(mousePos.x,mousePos.y);
+  context.lineTo(p3.x,p3.y);
+  context.lineTo(p4.x,p4.y);
+  context.fillStyle = "rgba(255, 255, 255, 0.7)";
+  context.fill();
 
-    context.beginPath();
-    context.moveTo(mousePos.x, mousePos.y);
-    context.lineTo(center.x,center.y);
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(mousePos.x,mousePos.y);
-    context.lineTo(p1.x,p1.y);
-    context.lineTo(p2.x,p2.y);
-    context.fillStyle = "rgba(255, 255, 255, 0.7)";
-    context.fill();
+  context.beginPath();
+  context.arc(beamCenter.x, beamCenter.y, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = "rgba(255, 255, 255, 0.7)";
+  context.fill();
 
 
   canvas.onmousemove = function (e) {
     mousePos = windowToCanvas(canvas, e.clientX, e.clientY);
 
     vector = {
-      x: center.x - mousePos.x,
-      y: center.y - mousePos.y
+      x: beamCenter.x - mousePos.x,
+      y: beamCenter.y - mousePos.y
     }
 
     p1 = {
@@ -88,34 +122,49 @@ $( document ).ready(function() {
       y: (p1.y + p2.y)/2
     }
 
-    perpVector = {
-      x: (-(p2.y - p1.y))/2,
-      y: (p2.x - p1.x)/2
+    radius = getDist(p1.x, p1.y, p2.x, p2.y)/2;
+
+    dist = {
+      x: beamCenter.x - mousePos.x,
+      y: beamCenter.y - mousePos.y
     }
 
+    dd = Math.sqrt(dist.x * dist.x + dist.y * dist.y);
+
+    a = Math.asin(radius / dd);
+    b = Math.atan2(dist.y, dist.x);
+
+    t = b - a;
+    ta = { 
+      x:radius * Math.sin(t), 
+      y:radius * -Math.cos(t) 
+    };
+      
+    t = b + a;
+    tb = { 
+      x:radius * -Math.sin(t), 
+      y:radius * Math.cos(t) 
+    };
+
+      eraseBackground();
+      
     p3 = {
-      x: beamCenter.x + perpVector.x,
-      y: beamCenter.y + perpVector.y,
+      x: beamCenter.x + ta.x,
+      y: beamCenter.y + ta.y
     }
 
     p4 = {
-      x: beamCenter.x - perpVector.x,
-      y: beamCenter.y - perpVector.y,
+      x: beamCenter.x + tb.x,
+      y: beamCenter.y + tb.y
     }
 
-    eraseBackground();
+
     context.beginPath();
     context.moveTo(mousePos.x, mousePos.y);
     context.lineTo(center.x,center.y);
     context.stroke();
-
+    
     context.beginPath();
-    context.moveTo(mousePos.x,mousePos.y);
-    context.lineTo(p1.x,p1.y);
-    context.lineTo(p2.x,p2.y);
-    context.fillStyle = "rgba(255, 255, 255, 0.7)";
-    context.fill();
-
     context.moveTo(mousePos.x,mousePos.y);
     context.lineTo(p3.x,p3.y);
     context.lineTo(p4.x,p4.y);
@@ -123,7 +172,7 @@ $( document ).ready(function() {
     context.fill();
 
     context.beginPath();
-    context.arc(beamCenter.x, beamCenter.y, getDist(p1.x, p1.y, p2.x, p2.y)/2, 0, 2 * Math.PI, false);
+    context.arc(beamCenter.x, beamCenter.y, radius, 0, 2 * Math.PI, false);
     context.fillStyle = "rgba(255, 255, 255, 0.7)";
     context.fill();
 
