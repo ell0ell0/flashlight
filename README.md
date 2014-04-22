87ce76f3546a14be2566f1e93a4ed3ca8cc28d13
Lee Williams <williamsl@CFPB-rMBP15-Williams.(none)>
2014-04-22 12:50:09 -0400

    added content

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-flashlight
-==========
+# Capital Framework
+
+Master repository for building the complete Capital Framework.
+
+## Getting Started
+
+You can use Capital Framework in two ways:
+
+1. Download the compiled base framework with all included packages and just start coding.
+2. Get the source of the framework and build your own custom implementation of it.
+   * _Requires some comfortability with the command line (Terminal). Some experience with Grunt a plus._
+
+### "I just want to get the stuff and make something quickly"
+
+1. Download the zip file of the [latest release](#), which contains the contents of the `dist/` folder.
+2. Unzip it.
+3. Get going!
+
+### "I want to build my own"
+
+#### Requirements
+
+- [Grunt](http://gruntjs.com/) (and its dependencies)
+- That's it! Grunt will help you install everything else you need.
+
+#### Workflow
+
+1. Clone the repo and `cd` into its root
+2. `npm install` – Initializes Grunt in this folder and installs dependencies.
+3. `grunt vendor` – Pulls in Bower components.
+4. `grunt compile` (or just `grunt`) – Compiles LESS files, concatenates and minifies JS files, and rewrites image
+   and font asset paths.
+5. Develop your project in the `src` folder. Add your project's LESS in `src/static/css` and JS in `src/static/js`.
+6. Repeat step 4 to recompile LESS. (Or optionally setup the watch task to recompile on the fly.)
+
+#### Adding/removing/updating Bower components
+
+1. Edit the list of dependencies in `bower.json`.
+2. Re-run `grunt vendor`.
+3. Make any needed Gruntfile/template adjustments for consuming the new component (see below).
+4. Re-run `grunt compile`.
+
+##### Extra steps for components that contain production assets like fonts and images:
+
+1. Update `exportsOverride` in `package.json` to move the componenets asset types to the predefined asset
+   directories, like `/static/fonts/`.
+2. If this new package has CSS linking to fonts or images, the paths to them will automatically be rewritten as a
+   root relative link like `/static/fonts/`. If you have other asset types you will need to update the
+   `string-replace` task in Gruntfile.js.
+
+## Documentation
+
+More thorough documentation coming soon.
+
+## Contributing
+
+The project is in the public domain within the United States, and
+copyright and related rights in the work worldwide are waived through
+the [CC0 1.0 Universal public domain dedication](http://creativecommons.org/publicdomain/zero/1.0/).
+
+All contributions to this project will be released under the CC0
+dedication. By submitting a pull request, you are agreeing to comply
+with this waiver of copyright interest.
+
+## Release History
+
+ * 2013-12-16   [v0.3.1](../../tree/v0.3.1)   Update README; small fixes.
+ * 2013-12-16   [v0.3.0](../../tree/v0.3.0)   Enable fj- components with JS files in them / more complex `src` structures.
+ * 2013-12-16   [v0.2.2](../../tree/v0.2.2)   Add grunt-contrib-compress and grunt-release scripts. Create initial compress task. Add typography demo.
+ * 2013-12-13   [v0.2.1](../../tree/v0.2.1)   Add expandables component to `bower.json` dependencies.
+ * 2013-12-06   [v0.2.0](../../tree/v0.2.0)   More intelligent directing of assets to `src` and `dist`. Add test page in both `src` and `dist`. Can view with uniminified CSS/JS in `src`. Automatic copying of Bower component assets and replacement of paths in the compiled CSS. Set up JavaScript tasks for both end of `body` and in the `head`.
+ * 2013-11-08   [v0.1.3](../../tree/v0.1.3)   Add new fonts that went into `/dist`.
+ * 2013-11-08   [v0.1.2](../../tree/v0.1.2)   Improvements to Bower asset management.
+ * 2013-11-08   [v0.1.1](../../tree/v0.1.1)   Add new meta files.
+ * 2013-11-08   [v0.1.0](../../tree/v0.1.0)   First commit with grunt-cfpb-internal.
+
+## License
+
+The project is in the public domain within the United States, and
+copyright and related rights in the work worldwide are waived through
+the [CC0 1.0 Universal public domain dedication](http://creativecommons.org/publicdomain/zero/1.0/).
+
+All contributions to this project will be released under the CC0
+dedication. By submitting a pull request, you are agreeing to comply
+with this waiver of copyright interest.
+
+Software source code previously released under an open source license and then modified by CFPB staff is considered a "joint work" (see 17 USC § 101); it is partially copyrighted, partially public domain, and as a whole is protected by the copyrights of the non-government authors and must be released according to the terms of the original open-source license.
+
+For further details, please see: http://www.consumerfinance.gov/developers/sourcecodepolicy/
+
+---
+
+*This file was generated on Mon Jan 06 2014 10:57:20.*
---
+++ src/static/js/app.js
@@ -0,0 +1,238 @@
+$( document ).ready(function() {  
+  var canvas =  document.querySelector('#canvas'),
+                  context = canvas.getContext('2d');
+                  context.canvas.width  = $(window).width();
+                  context.canvas.height = $(window).height();
+
+  var center, mousePos, vector, p1, p2, p3, p4, radius, beamCenter;
+
+  var angle = 18;
+  var blur = 1;
+
+  center = { 
+    x: context.canvas.width/2,
+    y: context.canvas.height/2
+  };
+
+  mousePos = {
+      x: context.canvas.width - context.canvas.width/6,
+      y: context.canvas.height/4
+    };
+
+  vector = {
+    x: center.x - mousePos.x,
+    y: center.y - mousePos.y
+  }
+
+  p1 = {
+    x: center.x + vector.x * Math.cos(getRadians(angle)),
+    y: center.y + vector.y * Math.sin(getRadians(angle))
+  }
+
+  p2 = {
+    x: center.x + vector.x * Math.sin(getRadians(angle)),
+    y: center.y + vector.y * Math.cos(getRadians(angle))
+  }
+
+  beamCenter = {
+    x: (p1.x + p2.x)/2,
+    y: (p1.y + p2.y)/2
+  }
+
+  radius = getDist(p1.x, p1.y, p2.x, p2.y)/2;
+
+  dist = {
+    x: beamCenter.x - mousePos.x,
+    y: beamCenter.y - mousePos.y
+  }
+
+ //Calculate Tangents
+  var pointDistance = {
+      x: beamCenter.x - mousePos.x,
+      y: beamCenter.y - mousePos.y,
+      length: function () {
+          return Math.sqrt(this.x * this.x + this.y * this.y)
+      }
+  }
+
+  //Alpha
+  var a = Math.asin(radius / pointDistance.length());
+  //Beta
+  var b = Math.atan2(pointDistance.y, pointDistance.x);
+  //Tangent angle
+  var t = b - a;
+  //Tangent points
+  var T1 = {
+      x: beamCenter.x + radius * Math.sin(t),
+      y: beamCenter.y + radius * -Math.cos(t)
+  };
+
+  t = b + a;
+  var T2 = {
+      x: beamCenter.x + radius * -Math.sin(t),
+      y: beamCenter.y + radius * Math.cos(t)
+  }
+
+
+  context.save();
+
+  eraseBackground();
+
+  context.beginPath();
+  context.rect(0,0,context.canvas.width,context.canvas.height);
+  context.fillStyle = "#002D72";
+  context.fill();
+
+  context.beginPath();
+  context.moveTo(mousePos.x,mousePos.y);
+  context.lineTo(T1.x,T1.y);
+  context.lineTo(T2.x,T2.y);
+  context.fillStyle = "rgba(255, 255, 255, 0.7)";
+  context.fill();
+
+  context.globalCompositeOperation = 'xor';
+
+  context.beginPath();
+  context.shadowBlur = 10;
+  context.arc(beamCenter.x, beamCenter.y, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = "#FFF";
+  context.fill();
+
+  context.restore();
+
+  context.beginPath();
+  context.arc(beamCenter.x, beamCenter.y, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = "rgba(255, 255, 255, 0.3)";
+  context.fill();
+
+
+
+  canvas.onmousemove = function (e) {
+    mousePos = windowToCanvas(canvas, e.clientX, e.clientY);
+
+    vector = {
+      x: beamCenter.x - mousePos.x,
+      y: beamCenter.y - mousePos.y
+    }
+
+    p1 = {
+      x: center.x + vector.x * Math.cos(getRadians(angle)),
+      y: center.y + vector.y * Math.sin(getRadians(angle))
+    }
+
+    p2 = {
+      x: center.x + vector.x * Math.sin(getRadians(angle)),
+      y: center.y + vector.y * Math.cos(getRadians(angle))
+    }
+
+    beamCenter = {
+      x: (p1.x + p2.x)/2,
+      y: (p1.y + p2.y)/2
+    }
+
+    radius = getDist(p1.x, p1.y, p2.x, p2.y)/2;
+
+    //Calculate Tangents
+    pointDistance = {
+        x: beamCenter.x - mousePos.x,
+        y: beamCenter.y - mousePos.y,
+        length: function () {
+            return Math.sqrt(this.x * this.x + this.y * this.y)
+        }
+    }
+    //Alpha
+    a = Math.asin(radius / pointDistance.length());
+    //Beta
+    b = Math.atan2(pointDistance.y, pointDistance.x);
+    //Tangent angle
+    t = b - a;
+    //Tangent points
+    T1 = {
+        x: beamCenter.x + radius * Math.sin(t),
+        y: beamCenter.y + radius * -Math.cos(t)
+    };
+
+    t = b + a;
+    T2 = {
+        x: beamCenter.x + radius * -Math.sin(t),
+        y: beamCenter.y + radius * Math.cos(t)
+    }
+
+    context.save();
+
+    eraseBackground();
+
+    context.beginPath();
+  context.rect(0,0,context.canvas.width,context.canvas.height);
+  context.fillStyle = "#002D72";
+  context.fill();
+
+    context.beginPath();
+    context.moveTo(mousePos.x,mousePos.y);
+    context.lineTo(T1.x,T1.y);
+    context.lineTo(T2.x,T2.y);
+    context.fillStyle = "rgba(255, 255, 255, 0.7)";
+    context.fill();
+
+    context.globalCompositeOperation = 'xor';
+
+    context.beginPath();
+    context.arc(beamCenter.x, beamCenter.y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = "#FFF";
+    context.fill();
+
+    context.restore();
+
+    context.beginPath();
+    context.arc(beamCenter.x, beamCenter.y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = "rgba(255, 255, 255, 0.3)";
+    context.fill();
+
+
+  }
+
+  // ------------------------------------------------
+  // utilitits
+  // ------------------------------------------------
+  function map(value, inputMin, inputMax, outputMin, outputMax, clamp){
+    var outVal = ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
+    if( clamp ){
+      if(outputMax < outputMin){
+        if( outVal < outputMax )outVal = outputMax;
+        else if( outVal > outputMin )outVal = outputMin;
+      }else{
+        if( outVal > outputMax )outVal = outputMax;
+        else if( outVal < outputMin )outVal = outputMin;
+      }
+    }
+    return outVal;
+  }
+
+  function getDist(x1, y1, x2, y2) {
+    return Math.round( Math.sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2)) );
+  }
+
+  function getHypotenuse(x, y) {
+    return Math.sqrt( x*x + y*y );
+  }
+
+  function getRadians(degrees) {
+    return degrees*Math.PI/180;
+  }
+
+  function clamp(value, min, max) {
+    return value < min ? min : value > max ? max : value;
+  }
+
+  function windowToCanvas(canvas, x, y) {
+    var bbox = canvas.getBoundingClientRect();
+    return { x: x - bbox.left * (canvas.width  / bbox.width),
+             y: y - bbox.top  * (canvas.height / bbox.height)
+          };
+  }
+
+  function eraseBackground() {
+    context.clearRect(0,0,canvas.width,canvas.height);
+  }
+
+});
