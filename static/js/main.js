43f0e511279fcd8aa81aa1e61409189f9d786b8c
Lee Williams <williamsl@CFPB-rMBP15-Williams.(none)>
2014-04-23 11:36:46 -0400

    taking a look at the client bounding box

--- static/js/main.js
+++ static/js/main.js
@@ -10943,6 +10943,7 @@ $( document ).ready(function() {
 
     function windowToCanvas(canvas, x, y) {
       var bbox = canvas.getBoundingClientRect();
+      console.log(bbox);
       return { x: x - bbox.left * (canvas.width  / bbox.width),
                y: y - bbox.top  * (canvas.height / bbox.height)
             };
